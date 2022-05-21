4c79ebeab5c1285361cdb9cc7026c074cc9fc839
Guillermo Rubio Gutierrez <guillermorubiogutierrez@MacBook-Air-de-Guillermo.local>
2022-05-21 22:56:34 +0200

    [ADD] Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,9 +2,29 @@ import React from "react";
 import ChatBotFlow from "./ChatBotFlow/ChatBotFlow";
 
 const Footer = () => {
-  return <footer>
-    <ChatBotFlow />
-  </footer>;
+  return (
+    <footer>
+      {/* <ChatBotFlow /> */}
+      <div className="">
+        <div>
+          <p>CONÓCENOS</p>
+          <ul>
+            <li>Transparencia</li>
+            <li>Preguntas frecuentes</li>
+          </ul>
+        </div>
+        <div>
+          <p>LEGAL</p>
+          <ul>
+            <li>Términos y condiciones</li>
+            <li>Política y privacdad</li>
+            <li>Política de cookies</li>
+            <li>Aviso legal</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
