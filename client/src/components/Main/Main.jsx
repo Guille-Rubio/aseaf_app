8fe17e9718df4754daa63118da8fc59b6d36a16f
Guille-Rubio <guillermorubiog@gmail.com>
2022-05-21 02:47:37 +0200

    ADD home

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home/Home';
 
+
+
 const Main = () => {
   return <main>
     <Routes>
       <Route element={<Home />} path='/' />`
-
     </Routes>
+    
   </main>;
 };
 
