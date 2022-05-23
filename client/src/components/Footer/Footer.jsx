4a0c7cc59be577bf7fef861ae89638fd411efcb2
Guille-Rubio <guillermorubiog@gmail.com>
2022-05-23 18:17:49 +0200

    FIX removed APIRequest component from footer for deoloyment

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import Chatbot from "react-chatbot-kit";
 import config from "../../utils/chatbotConfig/config";
 import MessageParser from '../../utils/chatbotConfig/MessageParser';
 import ActionProvider from "../../utils/chatbotConfig/ActionProvider";
-import APIRequest from "../../utils/chatbotConfig/widgets/APIRequest";
+
 
 
 const Footer = () => {
@@ -47,7 +47,7 @@ const Footer = () => {
           <div className="footer__bottom--social--icons"><FaFacebookF size={20} /><FaTwitter size={20} /><FaInstagram size={20} /><FaYoutube size={20} /> </div>
         </div>
       </div>
-      <APIRequest/>
+      {/* <APIRequest/> */}
 
       <button className="chat-button" onClick={handleChatDisplay}>Habla con Nexus</button>
       {displayChat ?
