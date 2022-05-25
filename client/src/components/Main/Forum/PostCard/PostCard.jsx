e7e93af67a507b55bffe59db71779b28580b4b2d
Guillermo Rubio Gutierrez <guillermorubiogutierrez@MacBook-Air-de-Guillermo.local>
2022-05-25 17:59:23 +0200

    [ADD] Forum styles

--- client/src/components/Main/Forum/PostCard/PostCard.jsx
+++ client/src/components/Main/Forum/PostCard/PostCard.jsx
@@ -1,28 +1,44 @@
 import React from "react";
 import { FaRegHeart, FaRegComment, FaRegBookmark } from 'react-icons/fa';
+import diana from '../../../../assets/img/diana.png';
+import jaime from '../../../../assets/img/jaime.png';
+import laura from '../../../../assets/img/laura.png';
+import maria from '../../../../assets/img/maria.png';
+import pedro from '../../../../assets/img/pedro.png';
 
 const PostCard = (props) => {
+  const avatars = [diana, jaime, laura, maria, pedro];
+  const names = ['diana', 'jaime', 'laura', 'maria', 'pedro']
+
   const { title, image, author, message, response } = props.post;
   return (
-    <div>
-      {title}
-      <div>
-        <div>
-          <img src={image} />
+    <div className="postcard__container">
+      <p className="postcard__title">{title}</p>
+      <div className="postcard">
+        <div className="postcard__header">
+          {names.map((name, i) => {
+            if (name === image)
+              return <img className="postcard__avatar" src={avatars[i]} alt={author} />
+          })}
           {author}
         </div>
-        <div>
+        <div className="postcard__message">
           {message}
         </div>
-        <div>
+        <div className="postcard__icons">
           <FaRegBookmark /><FaRegHeart /><FaRegComment />
         </div>
+      </div>
+      <div>
         {response.length > 0
           ? response.map(resp => {
             return (
               <div>
                 <div>
-                  <img src={resp.image} />
+                  {names.map((name, i) => {
+                    if (name === resp.image)
+                      return <img src={avatars[i]} alt={author} />
+                  })}
                   {resp.author}
                 </div>
                 <div>
